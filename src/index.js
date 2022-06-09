eb20431befc9ae71985edb74c4c1a222237001f3
Aram Anto <aram.anto@gmail.com>
2022-06-09 15:34:31 +0200

    defined skeleton and configured webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+const something = document.createElement("div");
+something.id = "something";
+something.textContent = "That's something";
+document.body.append(something);
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Restaurant",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
